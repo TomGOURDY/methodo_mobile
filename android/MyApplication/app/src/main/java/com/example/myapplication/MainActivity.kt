3121901e16db032121e696e6ffaf1894cf5825aa
TomGourdy <tom.gourdy@edu.devinci.fr>
2021-04-09 09:16:17 +0200

    refresh repository

---
+++ android/MyApplication/app/src/main/java/com/example/myapplication/MainActivity.kt
@@ -0,0 +1,53 @@
+package com.example.myapplication
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.util.Log
+import android.view.ContextThemeWrapper
+import android.view.ViewGroup
+import android.widget.LinearLayout
+import android.widget.TextView
+
+class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private val INPUT_BUTTONS = listOf(
+                listOf("1", "2", "3", "/"),
+                listOf("4", "5", "6", "*"),
+                listOf("7", "8", "9", "-"),
+                listOf("0", ".", "=", "+"),
+        )
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        addCells(findViewById(R.id.calculator_input_container_line1), 0)
+        addCells(findViewById(R.id.calculator_input_container_line2), 1)
+        addCells(findViewById(R.id.calculator_input_container_line3), 2)
+        addCells(findViewById(R.id.calculator_input_container_line4), 3)
+    }
+
+    private fun addCells(linearLayout: LinearLayout, position: Int) {
+        for (x in INPUT_BUTTONS[position].indices) {
+            linearLayout.addView(
+                    TextView(
+                            ContextThemeWrapper(this, R.style.CalculatorInputButton)
+                    ).apply {
+                        text = INPUT_BUTTONS[position][x]
+                        setOnClickListener { onCellClicked(this.text.toString()) }
+                    },
+                    LinearLayout.LayoutParams(
+                            0,
+                            ViewGroup.LayoutParams.MATCH_PARENT,
+                            1f
+                    )
+            )
+        }
+    }
+
+    private fun onCellClicked(value: String) {
+        Log.d("CELL", value)
+    }
+}
---
+++ react/Calculator/App.js
@@ -0,0 +1,21 @@
+import React, {
+  Component
+ } from 'react';
+ import {
+ View,
+ Text,
+ AppRegistry
+ } from 'react-native';
+ import Style from './Style';
+ class ReactCalculator extends Component {
+   render() {
+       return (
+        <View style={Style.rootContainer}>
+        <View style={Style.displayContainer}></View>
+        <View style={Style.inputContainer}></View>
+    </View>
+ );
+ }
+ }
+ 
+ export default ReactCalculator
